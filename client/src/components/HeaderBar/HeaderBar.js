3cd0dc25d491978638c917e3db86e2c7943e6509
njohnstud <nicholas.johnston94@gmail.com>
2018-01-12 16:05:22 -0800

    configured header buttons

--- client/src/components/HeaderBar/HeaderBar.js
+++ client/src/components/HeaderBar/HeaderBar.js
@@ -1,26 +1,27 @@
 import React, { Component } from "react";
 
 import AppBar from "material-ui/AppBar";
-import IconMenu from "material-ui/IconMenu";
-import IconButton from "material-ui/IconButton";
 import FontIcon from "material-ui/FontIcon";
-import NavigationExpandMoreIcon from "material-ui/svg-icons/navigation/expand-more";
 import MenuItem from "material-ui/MenuItem";
-import DropDownMenu from "material-ui/DropDownMenu";
 import RaisedButton from "material-ui/RaisedButton";
-import {
-  Toolbar,
-  ToolbarGroup,
-  ToolbarSeparator,
-  ToolbarTitle
-} from "material-ui/Toolbar";
 import SelectField from "material-ui/SelectField";
 
 import Logo from "../../images/boomtown-logo.svg";
 
 import "./styles.css";
 
-const boomtownLogo = () => {};
+const Buttons = () => {
+  <div>
+    <RaisedButton label="My Profile" primary={true} />
+    <RaisedButton
+      style={{
+        backgroundColor: "#000"
+      }}
+      label="Logout"
+      primary={true}
+    />
+  </div>;
+};
 
 export default class HeaderBar extends Component {
   render() {
@@ -30,21 +31,33 @@ export default class HeaderBar extends Component {
         style={{
           backgroundColor: "#fff"
         }}
-        // iconElementLeft={{ boomtownLogo }}
-      >
-        <a href="/">
-          <img src={Logo} href={"/"} className="headerLogo" />
-        </a>
-        <SelectField floatingLabelText="Filter by Tag" />
-        <RaisedButton label="My Profile" primary={true} />
-        <RaisedButton
-          style={{
-            backgroundColor: "#000"
-          }}
-          label="Logout"
-          primary={true}
-        />
-      </AppBar>
+        iconElementLeft={
+          <div>
+            <a href="/">
+              <img src={Logo} href={"/"} className="headerLogo" />
+            </a>
+            <SelectField floatingLabelText="Filter by Tag" />
+          </div>
+        }
+        iconElementRight={
+          <div className="headerButtonWrapper">
+            <RaisedButton
+              className="headerButton"
+              label="My Profile"
+              primary={true}
+            />
+            <RaisedButton
+              className="headerButton"
+              style={{
+                backgroundColor: "#000"
+              }}
+              label="Logout"
+              primary={true}
+            />
+          </div>
+        }
+        // iconElementRight={<Buttons />}
+      />
     );
   }
 }
