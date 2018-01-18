44d57058e509a8d29396434cdc9161852b1ff116
njohnstud <nicholas.johnston94@gmail.com>
2018-01-18 09:24:43 -0800

    commit before entering the tunnel of death called redux, good times

--- client/src/containers/Profile/Profile.js
+++ client/src/containers/Profile/Profile.js
@@ -2,14 +2,16 @@ import React, { Component } from "react";
 
 import ItemCard from "../../components/ItemCard";
 import Items from "../Items/Items";
+import ProfileContainer from "./ProfileContainer";
 
 import Paper from "material-ui/Paper";
 
 import "../Profile/styles.css";
 
-const Profile = ({ list }) => {
+const Profile = ({ list, item, user }) => {
   return (
     <div className="profile">
+      <Paper />
       <Items list={list} />
     </div>
   );
