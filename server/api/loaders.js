ff087eae1952a5537603a9b9710e60c432d4bd6d
njohnstud <nicholas.johnston94@gmail.com>
2018-01-28 22:42:57 -0800

    added jsonServer.js file in /api to contain the fetch functions

--- server/api/loaders.js
+++ server/api/loaders.js
@@ -1,6 +1,11 @@
 import DataLoader from 'dataloader';
 import { getUserOwnedItems } from './jsonServer';
 
+// A batch loading function accepts an Array of keys, and returns a Promise which resolves to an Array of values. There are a few constraints that must be upheld:
+
+// The Array of values must be the same length as the Array of keys.
+// Each index in the Array of values must correspond to the same index in the Array of keys.
+
 export default function() {
   return {
     UserOwnedItems: new DataLoader(ids => (
